98b58ef6e356df4f660041842ece666015fbc574
Guillenfe <guillermopc2014@gmail.com>
2026-03-29 00:57:31 +0100

    wishlist changes

--- TFG/cinesnap/src/components/Wishlist/Wishlist.jsx
+++ TFG/cinesnap/src/components/Wishlist/Wishlist.jsx
@@ -7,14 +7,17 @@ function Wishlist() {
     {
       id: 1,
       title: "Inception",
+      date: 2010,
       image: "https://via.placeholder.com/150x220",
       stores: [
         {
+          logo: "https://cdn-gdjgd.nitrocdn.com/puszgbaFBTTMTmzNUiCrRdNAekkabGtJ/assets/images/optimized/rev-01693b6/policyviz.com/wp-content/uploads/2020/12/amazon-logo-square-285x300.jpg",
           name: "Amazon",
           price: 11.2,
           link: "https://www.amazon.es/Origen-Blu-ray-Gordon-Levitt-Leonardo-DiCaprio/dp/B08LB2K9GK/ref=sr_1_2?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2CJDRDLKP4U20&dib=eyJ2IjoiMSJ9.xhhFE3n6UKNaUOhmSWXQlQWtGpFgpglImXK-aVZFqPxYCe8kPYUpfg1sCEgqcfUmPV9HA0k5BMhb4v3u7BtZXHtpapjs2CG4i7pWHgG60upijSn6NCm3uAKoKdQ_aBSvmYwEdBVaDQALqt-VNxT0gB-GZMb-xq46tHo7aqR6T3qe9yZkTBnjEO16lZ2ezISTwzE-asVUZonQRXPkhjYgO3ucas_FL89_3fW_twuaVqdQ7QpKV4QAcdC-4Gqf5hs1Q1os1fRlA1_A75JIhblmdXDadQq4w5waEShLDH9Ryvc.wN0xO5AzbDt6ho1xxcHw-IFodpX9eBceNaI5j8WepRE&dib_tag=se&keywords=inception&qid=1774347914&sprefix=inception%2Caps%2C406&sr=8-2",
         },
         {
+          logo: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Fnac_Logo.svg",
           name: "Fnac",
           price: 11.99,
           link: "https://www.fnac.es/a7797231/Origen-Blu-ray-Leonardo-DiCaprio",
@@ -24,14 +27,17 @@ function Wishlist() {
     {
       id: 2,
       title: "Interstellar",
+      date: 2014,
       image: "https://via.placeholder.com/150x220",
       stores: [
         {
+          logo: "https://cdn-gdjgd.nitrocdn.com/puszgbaFBTTMTmzNUiCrRdNAekkabGtJ/assets/images/optimized/rev-01693b6/policyviz.com/wp-content/uploads/2020/12/amazon-logo-square-285x300.jpg",
           name: "Amazon",
           price: 34.99,
           link: "https://www.amazon.es/HWC-Trading-Interstellar-Mcconaughey-Imprimieron/dp/B09DSV52DC/ref=sr_1_1_sspa?adgrpid=1309518783566525&dib=eyJ2IjoiMSJ9.hDjHPFA6ekNZbocosdB43xk_2TnhZkw1DvUpm_LMqgk37ATqwNwJKI21HGChLs4JUpFUJmQddO0nXpixhrZMhGUB1b0QwoPbEwRnAL1PBIUGzLO-lkf4vBGpeB3n0SalbkIww_pAXiP89kLoSkHTh1nxhvR3F7H9mKjkWPX1q-UVorZWICKiQxQdHzWuxkkbG1eAaYt-4ZEDmsltaMqsq1gT8CiaDWvoctsHLeYAAXmBo_x_D2VuFKz2uJ0U9uXR6WXsAy_p6fdDisGBilx680PDM3xg3FTM_E4TuiJOAII.k_iXWIMgH1jfXreXiq_gcQvY1cdzuGkZ9n6BkURUSwo&dib_tag=se&hvadid=81845012637226&hvbmt=bp&hvdev=c&hvlocphy=164580&hvnetw=o&hvqmt=p&hvtargid=kwd-81845158896748%3Aloc-170&hydadcr=14551_1862904&keywords=interstellar&mcid=108077b382a73a0bb59ab04b418da524&msclkid=b8d8710f656217c8293b18cd0324568a&qid=1774347850&sr=8-1-spons&aref=7TTvP7iVVg&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1",
         },
         {
+          logo: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Fnac_Logo.svg",
           name: "Fnac",
           price: 19.99,
           link: "https://www.fnac.es/a7725135/Interstellar-UHD-Blu-ray-Matthew-McConaughey",
@@ -63,6 +69,7 @@ function Wishlist() {
             <WishlistItem
               key={movie.id}
               title={movie.title}
+              date={movie.date}
               image={movie.image}
               stores={movie.stores}
               onRemove={() => removeMovie(movie.id)}
--- TFG/cinesnap/src/components/Wishlist/WishlistItem.jsx
+++ TFG/cinesnap/src/components/Wishlist/WishlistItem.jsx
@@ -1,46 +1,79 @@
 import { FaLink, FaTimes } from "react-icons/fa";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai"; // Iconos de corazón
 
-function WishlistItem({ title, image, stores, onRemove }) {
+function WishlistItem({ title, date, image, stores, onRemove }) {
+
+  const sortedStores = [...stores].sort((a, b) => a.price - b.price);
+
   return (
-    <div className="flex items-center bg-neutral-800 rounded-lg p-4 shadow-md w-full">
-      {/* Poster */}
+    <div className="group flex bg-neutral-800 rounded-lg p-4 shadow-md w-full relative">
+      {/* Poster 
       <img
         src={image || "https://via.placeholder.com/150x220"}
         alt={title}
         className="w-[100px] h-[150px] object-cover rounded-md"
       />
+      */}
+
+      <div className="w-[140px] h-[200px] object-cover bg-neutral-600">
+      </div>
 
       {/* Info */}
       <div className="flex-1 ml-6 text-left">
-        <h3 className="text-lg font-bold mb-2">{title}</h3>
+        <h3 className="text-xl font-bold mb-1">{title}</h3>
+        <h3 className="text-md mb-3">DVD {date}</h3>
 
         {/* Tiendas con links */}
-        <div className="space-y-1 text-sm">
-          {stores.map((store, index) => (
+        <div className="flex flex-col gap-1">
+          {sortedStores.map((store, index) => (
             <a
               key={index}
-              href={store.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block hover:underline hover:text-blue-400 cursor-pointer"
-            >
-              {store.name}:{" "}
-              <span className="font-semibold">{store.price}€</span> <FaLink className="inline ml-1" />
+              target="blank"
+              className="grid grid-cols-3 items-center bg-neutral-700 p-2 rounded-md"
+              href={store.link}>
+
+              {/* Logo y Nombre */}
+              <div className="flex items-center gap-3">
+                <img src={store.logo} alt={store.name} className="w-6 h-6 rounded" />
+                <span className="font-medium text-sm">{store.name}</span>
+              </div>
+
+              {/* Precio */}
+              <div className="text-center text-sm">
+                {store.price} €
+              </div>
+
+              {/* Botón */}
+              <div className="text-right">
+                <a href={store.link} target="blank" className="bg-red-600 hover:bg-red-700 text-white text-sm py-1.5 px-3 rounded-md transition-all">
+                  Comprar
+                </a>
+              </div>
             </a>
           ))}
         </div>
       </div>
 
-      {/* Botón eliminar */}
+      {/* Botón Wishlist */}
+      <button
+        onClick={onRemove}
+        className="absolute top-2 right-2 text-white text-2xl p-2.5 rounded-full bg-black/50 hover:bg-red-600 transition opacity-0 group-hover:opacity-100"
+        title="Quitarlo de la lista"
+      >
+        <AiFillHeart />
+      </button>
+      {/*
       <button
         onClick={onRemove}
         className="ml-4 flex items-center justify-center w-10 h-10 rounded-full 
              bg-neutral-700 text-white text-xl font-bold
              transition-all duration-200
-             hover:bg-red-600 hover:scale-110 hover:text-white"
+             hover:bg-red-600 hover:scale-110 hover:text-white
+             absolute -top-2 right-2"
       >
         <FaTimes />
       </button>
+      */}
     </div>
   );
 }
